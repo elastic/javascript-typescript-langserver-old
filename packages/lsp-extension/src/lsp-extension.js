c918d2ad110e06a50bf53428532aa0f55169b5a9
Mengwei Ding <mengwei.ding@gmail.com>
2018-08-17 16:27:07 +0800

    chore: update lsp-extension package setup

---
+++ packages/lsp-extension/src/lsp-extension.js
@@ -0,0 +1,10 @@
+"use strict";
+exports.__esModule = true;
+var ReferenceCategory;
+(function (ReferenceCategory) {
+    ReferenceCategory[ReferenceCategory["UNCATEGORIZED"] = 0] = "UNCATEGORIZED";
+    ReferenceCategory[ReferenceCategory["READ"] = 1] = "READ";
+    ReferenceCategory[ReferenceCategory["WRITE"] = 2] = "WRITE";
+    ReferenceCategory[ReferenceCategory["INHERIT"] = 3] = "INHERIT";
+    ReferenceCategory[ReferenceCategory["IMPLEMENT"] = 4] = "IMPLEMENT";
+})(ReferenceCategory = exports.ReferenceCategory || (exports.ReferenceCategory = {}));
